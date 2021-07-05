f97261382d3220a5433d4917b755cfa2182baabd
ildar <bajkinildar@gmail.com>
2021-07-05 18:55:46 +0400

    first version

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install ansible, git
+
+ansible-galaxy install andrewrothstein.kops
+ansible-galaxy install kostyrev.direnv
+
+cp -r $PWD/ildar.software /home/$USER/.ansible/roles
+
+ansible-playbook software.yml
+
+echo -n "Нужно удалить предустановленные роли из папки .ansible/roles (y/n)? "
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    
+	ansible-galaxy remove andrewrothstein.kops
+	ansible-galaxy remove kostyrev.direnv
+	ansible-galaxy remove ildar.software
+	echo "Installation complete."
+else
+    echo "Ok, leave them for you. Installation complete."
+fi
